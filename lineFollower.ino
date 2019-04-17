8d26dc554182db6d1c2f1f7bea6584fb3f242174
Erik Noorland <mail@eriknoorland.nl>
2019-04-17 21:16:45 +0200

    Inverse motors, Increase speed, Decrease number of line sensors

--- lineFollower.ino
+++ lineFollower.ino
@@ -1,13 +1,8 @@
-// shiftIn register
-#define LOAD_PIN 2
-#define CLOCK_ENABLE_PIN 3
-#define CLOCK_PIN 4
-#define DATA_PIN 5
-
-#define MOTOR_SPEED 100
+#define MOTOR_SPEED 150
 #define Kp 30
 #define Kd 3
 
+#define NUM_LINE_SENSORS 5
 #define SENSOR_LINE_VALUE 1 // 1 = black, 0 = white
 
 struct Motor {
@@ -16,8 +11,10 @@ struct Motor {
   int input2; // direction
 };
 
-Motor leftMotor = { 10, 6, 7 };
-Motor rightMotor = { 11, 8, 9 };
+Motor leftMotor = { 13, 12, 11 };
+Motor rightMotor = { 8, 10, 9 };
+
+int lineSensors[NUM_LINE_SENSORS] = { 7, 6, 5, 4, 3 }; // left to right
 
 int lastError = 0;
 long lastMillis = 0;
@@ -30,26 +27,25 @@ long lastMillis = 0;
  */
 void motorControl(Motor motor, int speed, int direction=1) {
   analogWrite(motor.enable, speed);
-  digitalWrite(motor.input1, (direction == 1 ? LOW : HIGH));
-  digitalWrite(motor.input2, (direction == 1 ? HIGH : LOW));
+  digitalWrite(motor.input1, (direction == 1 ? HIGH : LOW));
+  digitalWrite(motor.input2, (direction == 1 ? LOW : HIGH));
 }
 
 /**
  * Setup
  */
 void setup() {
-  pinMode(LOAD_PIN, OUTPUT);
-  pinMode(CLOCK_ENABLE_PIN, OUTPUT);
-  pinMode(CLOCK_PIN, OUTPUT);
-  pinMode(DATA_PIN, INPUT);
+  pinMode(leftMotor.enable, OUTPUT);
+  pinMode(leftMotor.input1, OUTPUT);
+  pinMode(leftMotor.input2, OUTPUT);
 
   pinMode(rightMotor.enable, OUTPUT);
   pinMode(rightMotor.input1, OUTPUT);
   pinMode(rightMotor.input2, OUTPUT);
 
-  pinMode(leftMotor.enable, OUTPUT);
-  pinMode(leftMotor.input1, OUTPUT);
-  pinMode(leftMotor.input2, OUTPUT);
+  for (int i = 0; i < NUM_LINE_SENSORS; i++) {
+    pinMode(lineSensors[i], INPUT);
+  }
 
   Serial.begin(9600);
 }
@@ -58,21 +54,27 @@ void setup() {
  * Loop
  */
 void loop() {
-  byte sensorData = getSensorData();
+  int lineSensorData[NUM_LINE_SENSORS] = {};
 
-  int goal = 4;
-  int activeSensor = getLineDetectionSensor(sensorData);
+  for (int i = 0; i < NUM_LINE_SENSORS; i++) {
+    lineSensorData[i] = digitalRead(lineSensors[i]);
+  }
+
+  int goal = 3;
+  int activeSensor = getActiveLineDetectionSensor(lineSensorData);
   int error = goal - activeSensor;
   int loopTime = millis() - lastMillis;
 
   float p = Kp * error;
   float d = Kd * ((error - lastError) / loopTime);
 
-  int leftMotorSpeed = MOTOR_SPEED + (p + d);
-  int rightMotorSpeed = MOTOR_SPEED - (p + d);
+  int leftMotorSpeed = MOTOR_SPEED - (p + d);
+  int rightMotorSpeed = MOTOR_SPEED + (p + d);
 
   leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
   rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
+
+  Serial.println(rightMotorSpeed);
 
   motorControl(leftMotor, leftMotorSpeed, 1);
   motorControl(rightMotor, rightMotorSpeed, 1);
@@ -84,36 +86,16 @@ void loop() {
 }
 
 /**
- * Returns the number of the sensor that detected the line
- * @param {byte} sensorData
+ * Returns the index of the sensor that detected the line
+ * @param {array} sensorData
  * @return {int}
  */
-int getLineDetectionSensor(byte sensorData) {
-  for (int i = 1; i < 8; i++) {
-    if ((bitRead(sensorData, i) == 0) == SENSOR_LINE_VALUE) {
-      return i;
+int getActiveLineDetectionSensor(int sensorData[]) {
+  for (int i = 0; i < NUM_LINE_SENSORS; i++) {
+    if (sensorData[i] == SENSOR_LINE_VALUE) {
+      return i + 1;
     }
   }
 
   return 0;
-}
-
-/**
- * Returns the sensor data
- * @return {byte} 1 == far right, 7 == far left
- */
-byte getSensorData() {
-  digitalWrite(LOAD_PIN, LOW);
-  delayMicroseconds(5);
-
-  digitalWrite(LOAD_PIN, HIGH);
-  delayMicroseconds(5);
-
-  digitalWrite(CLOCK_PIN, HIGH);
-  digitalWrite(CLOCK_ENABLE_PIN, LOW);
-
-  byte data = shiftIn(DATA_PIN, CLOCK_PIN, LSBFIRST);
-  digitalWrite(CLOCK_ENABLE_PIN, HIGH);
-
-  return data;
 }
