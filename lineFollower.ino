de824343f068e90109fcf9f14455fbcdde9079dd
Erik Noorland <mail@eriknoorland.nl>
2019-04-22 15:00:58 +0200

    Replace delay with millis counter, Add ultrasonic sensor for collision avoidance

--- lineFollower.ino
+++ lineFollower.ino
@@ -1,9 +1,13 @@
-#define MOTOR_SPEED 150
-#define Kp 30
+#include <TimerOne.h>
+
+#define MOTOR_SPEED 120
+#define Kp 25
 #define Kd 3
 
 #define NUM_LINE_SENSORS 5
 #define SENSOR_LINE_VALUE 1 // 1 = black, 0 = white
+
+#define FPS 50
 
 struct Motor {
   int enable; // speed
@@ -11,13 +15,24 @@ struct Motor {
   int input2; // direction
 };
 
+struct Ultrasonic {
+  int trigger;
+  int echo;
+};
+
 Motor leftMotor = { 13, 12, 11 };
 Motor rightMotor = { 8, 10, 9 };
 
+Ultrasonic frontUltrasonic = { 22, 2 };
+
 int lineSensors[NUM_LINE_SENSORS] = { 7, 6, 5, 4, 3 }; // left to right
+int lastActiveSensor;
 
 int lastError = 0;
 long lastMillis = 0;
+
+// long duration;
+volatile long distance;
 
 /**
  * Motor control
@@ -35,6 +50,9 @@ void motorControl(Motor motor, int speed, int direction=1) {
  * Setup
  */
 void setup() {
+  Timer1.initialize(10 * 1000);
+  Timer1.attachInterrupt(getFrontDistance);
+
   pinMode(leftMotor.enable, OUTPUT);
   pinMode(leftMotor.input1, OUTPUT);
   pinMode(leftMotor.input2, OUTPUT);
@@ -42,6 +60,9 @@ void setup() {
   pinMode(rightMotor.enable, OUTPUT);
   pinMode(rightMotor.input1, OUTPUT);
   pinMode(rightMotor.input2, OUTPUT);
+
+  pinMode(frontUltrasonic.trigger, OUTPUT);
+  pinMode(frontUltrasonic.echo, INPUT);
 
   for (int i = 0; i < NUM_LINE_SENSORS; i++) {
     pinMode(lineSensors[i], INPUT);
@@ -54,35 +75,49 @@ void setup() {
  * Loop
  */
 void loop() {
-  int lineSensorData[NUM_LINE_SENSORS] = {};
+  unsigned long currentMillis = millis();
 
-  for (int i = 0; i < NUM_LINE_SENSORS; i++) {
-    lineSensorData[i] = digitalRead(lineSensors[i]);
+  if (distance >= 2 && distance <= 400) {
+    if (distance <= 15) {
+      motorControl(leftMotor, 0, 1);
+      motorControl(rightMotor, 0, 1);
+      return;
+    }
   }
 
-  int goal = 3;
-  int activeSensor = getActiveLineDetectionSensor(lineSensorData);
-  int error = goal - activeSensor;
-  int loopTime = millis() - lastMillis;
+  if (currentMillis - lastMillis >= 1000 / FPS) {
+    int lineSensorData[NUM_LINE_SENSORS] = {};
 
-  float p = Kp * error;
-  float d = Kd * ((error - lastError) / loopTime);
+    for (int i = 0; i < NUM_LINE_SENSORS; i++) {
+      lineSensorData[i] = digitalRead(lineSensors[i]);
+    }
 
-  int leftMotorSpeed = MOTOR_SPEED - (p + d);
-  int rightMotorSpeed = MOTOR_SPEED + (p + d);
+    int goal = 3;
+    int activeSensor = getActiveLineDetectionSensor(lineSensorData);
 
-  leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
-  rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
+    if (activeSensor == 0) {
+      activeSensor = lastActiveSensor;
+    }
 
-  Serial.println(rightMotorSpeed);
+    int error = goal - activeSensor;
+    int loopTime = millis() - lastMillis;
 
-  motorControl(leftMotor, leftMotorSpeed, 1);
-  motorControl(rightMotor, rightMotorSpeed, 1);
+    float p = Kp * error;
+    float d = Kd * ((error - lastError) / loopTime);
 
-  lastError = error;
-  lastMillis = millis();
+    int leftMotorSpeed = MOTOR_SPEED - (p + d);
+    int rightMotorSpeed = MOTOR_SPEED + (p + d);
 
-  delay(20);
+    leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
+    rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
+
+    motorControl(leftMotor, leftMotorSpeed, 1);
+    motorControl(rightMotor, rightMotorSpeed, 1);
+
+    lastError = error;
+    lastMillis = millis();
+    lastActiveSensor = activeSensor;
+  }
 }
 
 /**
@@ -98,4 +133,19 @@ int getActiveLineDetectionSensor(int sensorData[]) {
   }
 
   return 0;
+}
+
+/**
+ * Returns the front distance in cm's
+ * @return
+ */
+long getFrontDistance() {
+  digitalWrite(frontUltrasonic.trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(frontUltrasonic.trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(frontUltrasonic.trigger, LOW);
+
+  long duration = pulseIn(frontUltrasonic.echo, HIGH);
+  distance = duration * 0.034 / 2;
 }
